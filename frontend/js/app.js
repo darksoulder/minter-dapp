01acf9533be37c6abeed7a70253a37215ec3d7af
darksoulder <xxcssplayaxx@gmx.de>
2022-10-15 19:40:06 +0200

    1

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -68,11 +68,11 @@ const updateConnectStatus = async () => {
     onboardButton.disabled = true;
     onboarding.stopOnboarding();
     /*notConnected.classList.remove('show-not-connected');
-    notConnected.classList.add('hidden');*/
+    notConnected.classList.add('hidden');
     // SHOW SPINNER
     spinner.classList.remove('hidden');
     window.contract = new web3.eth.Contract(abi, contractAddress);
-    loadInfo();
+    loadInfo();*/
   } else {
     onboardButton.innerText = "Connect MetaMask!";
     // HIDE SPINNER
