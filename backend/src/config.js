87196afbeb6cd7740f211ca1e203283ba8310f4c
darksoulder <xxcssplayaxx@gmx.de>
2022-10-16 00:44:41 +0200

    1

--- backend/src/config.js
+++ backend/src/config.js
@@ -39,7 +39,7 @@ const extraMetadata = {
 // ** REQUIRED **
 const AUTH = process.env.NFTPORT_API_KEY; // Set this in the .env file to prevent exposing your API key when pushing to Github
 const LIMIT = 2; // Your API key rate limit
-const CHAIN = 'goerli'; // only goerli, polygon, or ethereum
+const CHAIN = 'polygon'; // only goerli, polygon, or ethereum
 
 // REQUIRED CONTRACT DETAILS THAT CANNOT BE UPDATED LATER!
 const CONTRACT_NAME = 'KATEST';
