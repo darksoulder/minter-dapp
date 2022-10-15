cf7718ed753117bcf27bbec02a88bf5d9c29df90
darksoulder <xxcssplayaxx@gmx.de>
2022-10-15 19:45:31 +0200

    1

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -67,10 +67,10 @@ const updateConnectStatus = async () => {
     window.address = accounts[0];
     onboardButton.disabled = true;
     onboarding.stopOnboarding();
-    /*notConnected.classList.remove('show-not-connected');
+    notConnected.classList.remove('show-not-connected');
     notConnected.classList.add('hidden');
     // SHOW SPINNER
-    spinner.classList.remove('hidden');
+    /*spinner.classList.remove('hidden');
     window.contract = new web3.eth.Contract(abi, contractAddress);
     loadInfo();*/
   } else {
