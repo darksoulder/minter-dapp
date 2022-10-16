c342bf1d1cc70df628a1c6d54c095c4aa2b036ed
darksoulder <xxcssplayaxx@gmx.de>
2022-10-16 20:55:13 +0200

    1

--- backend/src/config.js
+++ backend/src/config.js
@@ -66,7 +66,7 @@ const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
-const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
+const GENERIC = false; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
 const GENERIC_TITLE = "KATRSRE"; // Replace with what you want the generic titles to say if you want it to be different from the contract name.
 const GENERIC_DESCRIPTION = "which will you get"; // Replace with what you want the generic descriptions to say.
 const GENERIC_IMAGE = "https://ipfs.io/ipfs/bafybeifp3qlfediqr4jxlh7vh6uae4fcogd5dgkgrxo32us3xr7bqgrvau"; // Replace with your generic image that will display for all NFTs pre-reveal.
