e4b7e3897161c7b50d05faab86ed908cb6d183be
darksoulder <xxcssplayaxx@gmx.de>
2022-10-17 23:09:56 +0200

    1

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "0x3fF876a3a286553A985645B2299E97834f57A957"; // Replace with your own contract address
+const contractAddress = "0x38B4BE8eacEcDd7F873EbFaBb2C9225Ce0F9fdAF"; // Replace with your own contract address
 const chain = 'goerli'; // goerli, polygon, or ethereum
 
 const welcome_h1 = "Welcome to the Kawaii Anime Girls Club";
