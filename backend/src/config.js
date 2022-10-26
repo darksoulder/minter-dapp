4d5e9ca75971602d9cbf584cff52d46d0234525b
darksoulder <xxcssplayaxx@gmx.de>
2022-10-26 16:06:58 +0200

    1

--- backend/src/config.js
+++ backend/src/config.js
@@ -13,7 +13,7 @@ const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
 
 const layerConfigurations = [
   {
-    growEditionSizeTo: 5,
+    growEditionSizeTo: 5000,
     layersOrder: [
       { name: "Unique Kawaii Anime Girl" },
     ],
